dbb736f00aec4a1b1cc706c095670cbb91ce7642
Yeongseo Kim <1903020@ewhain.net>
2025-03-14 14:08:27 +0900

    feat: Todo 상단에 오늘 날짜 반영 구현

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const addButton = document.getElementById('add-button')
 
 const todoList = []
 
+document.addEventListener('DOMContentLoaded', () => {
+  const yearElement = document.getElementById('daily-year')
+  const dateElement = document.getElementById('daily-date')
+
+  const dateObj = new Date()
+  const [day, month, date, year] = dateObj.toString().split(' ')
+
+  yearElement.innerHTML = year
+  dateElement.innerHTML = `${month} ${date} ${day}`
+})
+
 const handleAddTodo = () => {
   const task = input.value
 
